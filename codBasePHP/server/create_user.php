c7e1c1392c1261e269ba78e32386ac8d6b9d4a0b
Ericrondan <42428227+Ericrondan@users.noreply.github.com>
2019-12-20 17:11:13 -0300

    Add files via upload

---
+++ codBasePHP/server/create_user.php
@@ -0,0 +1,24 @@
+<?php
+
+    include 'conexionBD.php';
+    CrearUsuarios("Pablo Gonzales","1993/07/16","Pablo@email.com","12345");
+    CrearUsuarios("Maria Rodriguez","1992/01/16","Maria@email.com","123456");
+    CrearUsuarios("Roberto Gomez","1990/01/16","Robert@email.com","1234567");
+
+    function CrearUsuarios($Nombre,$FechaNacimiento,$UserName,$Password){
+    IniciarConexion();
+    $Consulta="Select * from usuario where Username='".$UserName."'";
+    $Resultado= mysqli_num_rows($GLOBALS['Conexion']->query($Consulta));
+    if($Resultado==0){
+        $Consulta = "INSERT INTO usuario (Nombre, FechaNacimiento, Username, Password)
+        VALUES ('".$Nombre."', '".$FechaNacimiento."', '".$UserName."', '".password_hash($Password, PASSWORD_DEFAULT)."')";
+
+        if ($GLOBALS['Conexion']->query($Consulta) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $GLOBALS['Conexion']->error;
+        }
+    }
+    DesactivarConexion();
+    }
+ ?>
---
+++ recursos/examen.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-12-2019 a las 18:38:26
+-- Versión del servidor: 10.1.40-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `examen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `Id` int(11) NOT NULL,
+  `IdUsuario` int(11) DEFAULT NULL,
+  `Titulo` varchar(20) NOT NULL,
+  `FechaInicio` date NOT NULL,
+  `HoraInicio` time DEFAULT NULL,
+  `FechaFinalizacion` date DEFAULT NULL,
+  `HoraFinalizacion` time DEFAULT NULL,
+  `DiaCompleto` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `FechaNacimiento` varchar(10) NOT NULL,
+  `Username` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
